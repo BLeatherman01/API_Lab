b25be5470b977d7afa87346e460d97b26bdc2469
brandon <bleatherman01@gmail.com>
2022-11-01 09:17:18 -0400

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using API_LAB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using static API_LAB.Models.Data;
+
+namespace API_LAB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        RedditDAL api = new RedditDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+ 
+            return View();
+        }
+        
+        public IActionResult Post()
+        {
+          Rootobject rp = api.GetPost();
+            
+            return View(rp);  
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RedditDAL.cs
@@ -0,0 +1,20 @@
+﻿
+
+using RestSharp;
+
+namespace API_LAB.Models
+{
+    public class RedditDAL
+    {
+       public Rootobject GetPost() 
+       { 
+        var client = new RestClient($"https://www.reddit.com/r/aww/.json");
+        var request = new RestRequest();
+        var response = client.GetAsync<Rootobject>(request);
+        Rootobject rp = response.Result;
+
+        return rp; 
+       }
+       
+    }
+}
